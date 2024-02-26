e887592260552046640eaf70009940b5a7ebb55a
ChrisPytel <chris_pytel@hotmail.com>
2024-02-26 17:08:52 -0500

    added test cases for 200 and 404

--- fetcher.js
+++ fetcher.js
@@ -21,28 +21,31 @@ const fetcher = function(url, localPath) {
   request(url, (error, response, body) => {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    //console.log('body:', body); // Print the HTML for the body
+    
+    if (response.statusCode === 404){ 
+      console.log(`page was not found`);
+    }
 
-    //
+    if (response.statusCode === 200){ 
+      console.log(`All good, no errors present from server to client`);
 
-    //takes our body and writes it to a new file asynchronously
-     fs.writeFile(localPath, body, err => {
-      if (err) {
-        console.error(`issue writing content`, err);
-      } else {
-       console.log(`file written successfully and saved to ${localPath}.`);
-      }
-      //checks the size
-      fs.stat(localPath, (err,stats)=>{
-        if (err){
-          console.log(`error occurred in checking file stats`, err);
+      //takes our body and writes it to a new file asynchronously
+      fs.writeFile(localPath, body, err => {
+        if (err) console.error(`issue writing content`, err);
+        else {
+        console.log(`file written successfully and saved to ${localPath}.`);
         }
-        else{
-          // console.log(`heres our super cool file stats`, stats);
-          console.log(`Our file has been sucessfully saved and takes up ${stats.size}bytes of memory.`);
-        }  
-      })
-    });
+        //checks the size
+        fs.stat(localPath, (err,stats)=>{
+          if (err) console.log(`error occurred in checking file stats`, err);        
+          else{
+            // console.log(`heres our super cool file stats`, stats);
+            console.log(`Our file has been sucessfully saved and takes up ${stats.size}bytes of memory.`);
+          }  
+        })
+      });     
+    }
+
   });
 
 
